74c3940a1c07dc213818ddbbd86088bb61bc0443
Robert Wohleb <rwohleb@gmail.com>
2014-10-16 03:08:23 -0700

    Added markdown_block plugin

---
+++ _plugins/markdown_block.rb
@@ -0,0 +1,23 @@
+#
+# Allows rendering blocks of content using markdown.
+#
+# Example:
+#
+# {% markdown %}
+# [Stack Overflow](http://www.stackoverflow.com)
+# {% endmarkdown %}
+#
+module Jekyll
+  class MarkdownBlock < Liquid::Block
+    def initialize(tag_name, text, tokens)
+      super
+    end
+    require "kramdown"
+    def render(context)
+      content = super
+      "#{Kramdown::Document.new(content).to_html}"
+    end
+  end
+end
+
+Liquid::Template.register_tag('markdown', Jekyll::MarkdownBlock)
